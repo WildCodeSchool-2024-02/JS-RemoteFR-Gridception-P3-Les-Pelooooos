a85b343487dd6b4918c20d2afe72169c9ccce450
Sébastien Creuzé <154002453+sebcreuze@users.noreply.github.com>
2024-06-13 14:57:01 +0200

    Update router.js

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -7,12 +7,15 @@ const router = express.Router();
 /* ************************************************************************* */
 
 const itemsRouter = require("./items/router");
+
 router.use("/items", itemsRouter);
 
 const plugsTypesRouter = require("./plugsTypes/router");
+
 router.use("/plugsTypes", plugsTypesRouter);
 
 const brandsRouter = require("./brands/router");
+
 router.use("/brands", brandsRouter);
 
 /* ************************************************************************* */
