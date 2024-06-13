21ae336f401e9369dded6f55365a46a8702592c2
Angélina Droz <angieso91@gmail.com>
2024-06-13 14:08:04 +0200

    CRUD/BREAD terminals table ok

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -6,10 +6,9 @@ const router = express.Router();
 // Import And Use Routers Here
 /* ************************************************************************* */
 
-const itemsRouter = require("./items/router");
+const terminalsRouter = require("./terminals/router");
 
-router.use("/items", itemsRouter);
-
+router.use("/terminals", terminalsRouter);
 /* ************************************************************************* */
 
 module.exports = router;
