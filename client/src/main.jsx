27f5516bd317ad0aaddac820584e50e0a1c8ee9e
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-06-14 10:27:42 +0200

    Route for connexion

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App";
 import Accueil from "./pages/Accueil";
 import Profil from "./pages/Profil";
 import Contact from "./pages/Contact";
+import Connexion from "./pages/Connexion";
 
 const router = createBrowserRouter([
   {
@@ -23,6 +24,10 @@ const router = createBrowserRouter([
       {
         path: "/contact",
         element: <Contact/>
+      },
+      {
+        path: "/connexion",
+        element: <Connexion/>
       },
     ],
   },
