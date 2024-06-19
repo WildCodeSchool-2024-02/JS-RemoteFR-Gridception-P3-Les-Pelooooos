cb9416b17903304beb0f1fa41fda0504924084ce
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-06-19 16:38:04 +0200

    Add first input email

--- client/src/components/InscriptionProfil.jsx
+++ client/src/components/InscriptionProfil.jsx
@@ -1,9 +1,54 @@
-import "../scss/Inscription.scss"
+import { useState } from "react";
+import "../scss/Inscription.scss";
 
-export default function NavBar() {
-    return (
-        <section>
-        <h1>INCRIPTION DU PROFIL </h1>
-        </section>
-    )
+export default function InscriptionProfil() {
+    const [inscription, setInscription] = useState({
+        email: "",
+      });
+    
+      const [error, setError] = useState("");
+    
+      const handleChangeForm = (event) => {
+        const { name, value } = event.target;
+        setInscription({ ...inscription, [name]: value });
+      };
+    
+      const isValidEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+      };
+      
+    
+      const togglePopup = () => {
+        if (!isValidEmail(inscription.email)) {
+          setError("L'adresse e-mail est invalide.");
+        } else {
+          setError("");
+          setInscription({
+            email: "",
+          });
+        }
+      };
+
+  return (
+    <section className="profilInformation">
+      <h2> INFORMATION DE VOTRE PROFIL</h2>
+      <form onSubmit={(event) => event.preventDefault()}>
+        <p>Email</p>
+        <input
+          type="email"
+          placeholder="email *"
+          name="email"
+          value={inscription.email}
+          onChange={(e) => handleChangeForm(e)}
+        />
+
+      </form>
+      {error && <p className="error">{error}</p>}
+      <button className="buttonIns" type="submit" onClick={togglePopup}>
+        VALIDER
+      </button>
+
+    </section>
+  );
 }
--- client/src/pages/Inscription.jsx
+++ client/src/pages/Inscription.jsx
@@ -5,16 +5,15 @@ import "../scss/Inscription.scss"
 import logoGeocode from "../assets/images/logo-geocode.png";
 
 export default function Inscription() {
+
   return (
-    <section>
+    <section className="inscription">
       <Link to="/">
         <img className="logoIns" src={logoGeocode} alt="Logo Geocode" />
       </Link>
-      <h1>INSCRIPTION</h1>
+      <h1 className="h1Ins">INSCRIPTION</h1>
       <InscriptionProfil />
-      <button type="submit">
-        SE CONNECTER
-      </button>
+      
     </section>
   );
 }
