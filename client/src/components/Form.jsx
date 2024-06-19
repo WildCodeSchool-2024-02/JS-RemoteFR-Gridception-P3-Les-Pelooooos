f5245206758aaa42c9b5dd829fdb4f1d298040e6
Angélina Droz <angieso91@gmail.com>
2024-06-19 12:32:45 +0200

    icon x insta fb ok

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -6,16 +6,16 @@ export default function Form() {
         <form action="">
     
             <label htmlFor="nom">Nom</label>
-            <input type="text" value="nom"/>
+            <input type="text" value="nom" required/>
     
             <label htmlFor="prénom">Prénom</label>
-            <input type="text" value="prénom"/>
+            <input type="text" value="prénom" required/>
     
             <label htmlFor="email">Email</label>
-            <input type="email" value="email"/>
+            <input type="email" value="email" required/>
     
             <label htmlFor="objet">Objet</label>
-            <select name="objet" id="objet">
+            <select name="objet" id="objet" required>
                 <option value="">Choisis ton objet</option>
                 <option value="information">Demande d'informations</option>
                 <option value="partenariat">Demande de partenariat</option>
@@ -23,9 +23,9 @@ export default function Form() {
             </select>
     
             <label htmlFor="message">Message</label>
-            <input type="text" value="message"/>
+            <input type="text" value="message" required/>
 
-            <input type="sublit" value="Envoyer le formulaire"/>
+            <input type="submit" value="Envoyer le formulaire"/>
 
         </form>
     )
