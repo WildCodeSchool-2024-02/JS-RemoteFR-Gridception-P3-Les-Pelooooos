7073f3446388a262ad6c04f52ee0d326170a40a9
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-06-19 11:44:31 +0200

    Create route for page Inscription

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,6 +8,7 @@ import Profil from "./pages/Profil";
 import Contact from "./pages/Contact";
 import Connexion from "./pages/Connexion";
 import Carte from "./pages/Carte";
+import Inscription from "./pages/Inscription";
 
 const router = createBrowserRouter([
   {
@@ -33,6 +34,10 @@ const router = createBrowserRouter([
       {
         path: "/carte",
         element: <Carte />,
+      },
+      {
+        path: "/inscription",
+        element: <Inscription />,
       },
     ],
   },
