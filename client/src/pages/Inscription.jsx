5d7bd0daaf8e2b0094203126b7c0a4e6c5717392
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-06-19 12:15:24 +0200

    Add first version of html for page inscription

--- client/src/pages/Inscription.jsx
+++ client/src/pages/Inscription.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
+import logoGeocode from "../assets/images/logo-geocode.png";
+
 export default function Inscription() {
-    return (
-        <div> Page d'inscription </div>
-    );
+  return (
+    <section>
+      <Link to="/">
+        <img className="logoId" src={logoGeocode} alt="Logo Geocode" />
+      </Link>
+      <h1 className="h1Id">CONNEXION</h1>
+      <button className="buttonId" type="submit">
+        SE CONNECTER
+      </button>
+    </section>
+  );
 }
