12bff51811557ccdac371f1bf273aa5d3b80a06f
unknown <sebastien.creuze@live.fr>
2024-06-26 16:17:41 +0200

    finished js of US

--- client/src/components/MajTerminals.jsx
+++ client/src/components/MajTerminals.jsx
@@ -1,8 +1,11 @@
+import upload from "../assets/images/icons-upload.png";
+
 export default function MajTerminals() {
   return (
-    <>
-      <h1>MAJ DES BORNES</h1>
-      <p>Uploader fichier CSV</p>
-    </>
+    <section className="listUsers">
+      <h1>MISE A JOUR DES BORNES</h1>
+      <p>Télécharger fichier CSV</p>
+      <img src={upload} alt="icons de téléchargement" />
+    </section>
   );
 }
