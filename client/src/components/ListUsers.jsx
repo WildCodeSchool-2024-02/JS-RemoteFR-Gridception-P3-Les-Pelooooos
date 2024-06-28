4e900a6d27d5fb1b3deb24711689a8f9b94f4d1c
unknown <sebastien.creuze@live.fr>
2024-06-28 15:36:11 +0200

    block to delete

--- client/src/components/ListUsers.jsx
+++ client/src/components/ListUsers.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
-// import axios from "axios";
+import axios from "axios";
 import { PropTypes } from "prop-types";
 import Cancel from "../assets/images/icons-cancel.png";
 import Down from "../assets/images/icons-down.png";
 import Up from "../assets/images/icons-up.png";
 
 export default function ListUsers({ users }) {
-  // const API_URL = import.meta.env.VITE_API_URL;
+  const API_URL = import.meta.env.VITE_API_URL;
+
   const [visibleCount, setVisibleCount] = useState(5);
+  const [user, setUser] = useState([]);
 
   const showMore = () => {
     setVisibleCount((prevCount) => prevCount + 5);
@@ -17,29 +19,39 @@ export default function ListUsers({ users }) {
     setVisibleCount(5);
   };
 
-  // const handleDelete = (user_id) => {
-  //   console.log(user_id);
-  //   axios.delete(`${API_URL}/api/users/${user_id}`).then((results) => {
-  //     if (results.status === 204) {
-  //       setUser(user.filter((user) => user.id !== user_id));
-  //     } else {
-  //       console.error(
-  //         "Une erreur est survenue, impossible de supprimer le client."
-  //       );
-  //     }
-  //   });
-  // };
+  const handleDelete = async (userId) => {
+    try {
+      const image = await axios.delete(`${API_URL}/api/images/${userId}`);
+      const cars = await axios.delete(`${API_URL}/api/cars/${userId}`);
+      // const reservation = await axios.delete(
+      //   `${API_URL}/api/reservations/${reservation_id}`
+      // );
+      const usersDelete = await axios.delete(`${API_URL}/api/users/${userId}`);
+
+      if (
+        image.status === 204 &&
+        cars.status === 204 &&
+        usersDelete.status === 204
+      ) {
+        setUser(user.filter((userA) => userA.id !== userId));
+      }
+    } catch (error) {
+      console.error(
+        "Une erreur est survenue, impossible de supprimer le client."
+      );
+    }
+  };
 
   return (
     <section className="listUsers">
       <h1>LISTES DES UTILISATEURS</h1>
-      {users.slice(0, visibleCount).map((user) => (
-        <p key={user.id}>
-          {user.lastname} || {user.firstname}
+      {users.slice(0, visibleCount).map((usere) => (
+        <p key={usere.id}>
+          {usere.lastname} || {usere.firstname}
           <button
             type="button"
             className="supression"
-            // onClick={() => handleDelete(user.id)}
+            onClick={() => handleDelete(usere.id)}
           >
             <img className="cancel" src={Cancel} alt="icon de suppression" />
           </button>
