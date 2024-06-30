44c9c7658f0442703c0b19c49dc7c5a584d40f7a
NICOLAS LOMBA <nicolaslomba82@gmail.com>
2024-06-30 16:50:05 +0200

    updating the localstorage

--- client/src/contexts/AuthContext.jsx
+++ client/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext, useMemo } from "react";
+import { createContext, useState, useContext, useMemo, useEffect } from "react";
 import PropTypes from "prop-types";
 
 const AuthContext = createContext(null);
@@ -15,6 +15,21 @@ export function AuthProvider({ children }) {
   function logout() {
     setAuth(null);
   }
+
+  useEffect (() => {
+    const storedAuth = localStorage.getItem("auth");
+    if (storedAuth) {
+      setAuth(JSON.parse(storedAuth));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (auth) {
+      localStorage.setItem("auth", JSON.stringify(auth));
+    } else {
+      localStorage.removeItem("auth");
+    }
+  }, [auth]);
 
   const value = useMemo(() => ({ auth, login, logout }), [auth]);
 
