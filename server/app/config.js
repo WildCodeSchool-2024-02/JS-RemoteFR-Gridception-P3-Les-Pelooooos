5c7cb935e999ebd44b7373c9bfae6c642f415763
unknown <sebastien.creuze@live.fr>
2024-07-15 10:59:17 +0200

    test deployt

--- server/app/config.js
+++ server/app/config.js
@@ -1,19 +1,38 @@
 const express = require("express");
+
 const cors = require("cors");
-const cookiesParser = require("cookie-parser");
-const apiRouter = require("./routers/api/router");
+
+const path = require("path");
 
 const app = express();
 
+app.use(cors());
+
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL],
+    origin: [
+      process.env.CLIENT_URL,
+      "http://mysite.com",
+      "http://another-domain.com",
+    ],
   })
 );
 
 app.use(express.json());
-app.use(cookiesParser());
+
+const apiRouter = require("./routers/api/router");
 
 app.use("/api", apiRouter);
+
+const reactBuildPath = path.join(__dirname, "/../../client/dist");
+const publicFolderPath = path.join(__dirname, "/../public");
+
+app.use(express.static(reactBuildPath));
+
+app.get("*.*", express.static(publicFolderPath, { maxAge: "1y" }));
+
+app.get("*", (_, res) => {
+  res.sendFile(path.join(reactBuildPath, "/index.html"));
+});
 
 module.exports = app;
